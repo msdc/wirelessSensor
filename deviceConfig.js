4144b2896a4962b9f8f004089160c47855aefa4f
yuzhongwusan <yuzhongwusan@users.noreply.github.com>
2014-08-14 18:46:18 +0800

    添加新文件。从原先的计算文件中移出
    添加新文件。。从原先的计算文件中移出

---
+++ deviceConfig.js
@@ -0,0 +1,11 @@
+exports.uuidArr=function(){
+    var pointSB=[];
+    var frontStr='E2C56DB5-DFFB-48D2-B060-D0F5A71096E0';
+        pointSB[frontStr+'_0_1']={x:0.00,y:5.00};//Cacc:'FE添加的mpgAjax中每个距离acc'
+        pointSB[frontStr+'_0_2']={x:5.00,y:0.00};
+        pointSB[frontStr+'_0_3']={x:0.00,y:0.00};
+        pointSB[frontStr+'_0_4']={x:5.00,y:5.00};
+        pointSB[frontStr+'_0_5']={x:2.50,y:2.30};
+        pointSB[frontStr+'_0_6']={x:10.00,y:5.00};
+    return pointSB;
+};
