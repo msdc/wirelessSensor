120a49d7135e85d09eeff759eaedf17fcb53a28f
donegal <murdercdh@hotmail.com>
2014-08-19 12:31:23 +0800

    update the logic for cal

---
+++ SensorCalculator.js
@@ -0,0 +1,65 @@
+/**
+ * Created by Administrator on 2014/8/19.
+ */
+var trilateration = require("./Trilateration.js");
+var kmeans = require("./KMeansClustering.js");
+
+/*One package of Sensor Data Calculate static class.*/
+function SensorDataCalculator() {
+};
+
+/*
+ 进行计算模块
+ */
+SensorDataCalculator.processCalculate = function (sourceData) {
+    var tpNDataArray = [];
+    var finalResult=[];
+    var dataObj;
+    try {
+        dataObj = JSON.parse(sourceData);
+    }
+    catch (e) {
+        return;
+    }
+    //exclude incorrect data.
+    if (!dataObj.deviceSerial) {
+        return;
+    }
+
+    tpNDataArray.push(dataObj);
+    var trlCal = new trilateration(tpNDataArray);
+    //清除空点
+    trlCal.cleanKeyZero();
+    //三点定一点
+    var levelOneResult=trlCal.getCalResult();
+    //聚类计算
+    if(levelOneResult&&levelOneResult.length>0){
+        for(var point in levelOneResult){
+            if(levelOneResult[point].beaconCalculatePosition) {
+                continue;
+            }
+            var levelTwoResult = kmeans.GetClusteredPoint(levelOneResult[point]);
+            finalResult.push((levelTwoResult));
+        }
+    }
+    return finalResult;
+/*    trlCal.cleanKeyZero(function (pointDt) {
+        for (var point in pointDt) {
+            if (!pointDt[point].beaconCalculatePosition) break;//skip incorrect data in redis.
+            kmeans.GetClusteredPoint(pointDt[point], function (finalPoint) {
+                //socket.emit('result',finalPoint);
+                //io.emit('result', finalPoint);
+               console.log("deviceID=" + finalPoint.deviceID);
+               console.log("timePoint=" + finalPoint.timePoint);
+               console.log("deviceSerial=" + finalPoint.deviceSerial);
+                console.log("beaconCanculatedPosition=[{\"x\"=" + finalPoint.beaconCalculatePosition[0].x + ",\"y=\"" + finalPoint.beaconCalculatePosition[0].y + "}]");
+                //todo write back info the redis and trigger postback event using websocket
+                //Save to the redis
+                //SensorDataCalculator.saveToRedis(finalPoint);
+            });
+        }
+    });*/
+};
+
+exports.SensorDataCalculator = SensorDataCalculator;
+
