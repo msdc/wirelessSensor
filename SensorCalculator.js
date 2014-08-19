2c07a62fa6189e0c782ccaa0a04a889ea55ce15c
donegal <murdercdh@hotmail.com>
2014-08-19 20:15:52 +0800

    Merge branch 'master' of https://github.com/msdc/wirelessSensor
    Conflicts:
    	app.io.js
    	public/demo.socket.io.client.html

--- SensorCalculator.js
+++ SensorCalculator.js
@@ -8,7 +8,7 @@ function SensorDataCalculator() {};
 
 SensorDataCalculator.processCalculate = function (sourceData) {
     var tpNDataArray = [];
-    var finalResult=[];
+    var finalResult = [];
     var dataObj;
     try {
         dataObj = JSON.parse(sourceData);
@@ -26,11 +26,11 @@ SensorDataCalculator.processCalculate = function (sourceData) {
     //清除空点
     trlCal.cleanZeroKey();
     //三点定一点
-    var levelOneResult=trlCal.getCalResult();
+    var levelOneResult = trlCal.getCalResult();
     //聚类计算
-    if(levelOneResult&&levelOneResult.length>0){
-        for(var point in levelOneResult){
-            if(levelOneResult[point].beaconCalculatePosition) {
+    if (levelOneResult && levelOneResult.length > 0) {
+        for (var point in levelOneResult) {
+            if (levelOneResult[point].beaconCalculatePosition) {
                 continue;
             }
             var levelTwoResult = kmeans.GetClusteredPoint(levelOneResult[point]);
@@ -38,43 +38,76 @@ SensorDataCalculator.processCalculate = function (sourceData) {
         }
     }
     return finalResult;
-/*    trlCal.cleanZeroKey(function (pointDt) {
-        for (var point in pointDt) {
-            if (!pointDt[point].beaconCalculatePosition) break;//skip incorrect data in redis.
-            kmeans.GetClusteredPoint(pointDt[point], function (finalPoint) {
-                //socket.emit('result',finalPoint);
-                //io.emit('result', finalPoint);
-               console.log("deviceID=" + finalPoint.deviceID);
-               console.log("timePoint=" + finalPoint.timePoint);
-               console.log("deviceSerial=" + finalPoint.deviceSerial);
-                console.log("beaconCanculatedPosition=[{\"x\"=" + finalPoint.beaconCalculatePosition[0].x + ",\"y=\"" + finalPoint.beaconCalculatePosition[0].y + "}]");
-                //todo write back info the redis and trigger postback event using websocket
-                //Save to the redis
-                //SensorDataCalculator.saveToRedis(finalPoint);
-            });
-        }
-    });*/
+    /*    trlCal.cleanZeroKey(function (pointDt) {
+     for (var point in pointDt) {
+     if (!pointDt[point].beaconCalculatePosition) break;//skip incorrect data in redis.
+     kmeans.GetClusteredPoint(pointDt[point], function (finalPoint) {
+     //socket.emit('result',finalPoint);
+     //io.emit('result', finalPoint);
+     console.log("deviceID=" + finalPoint.deviceID);
+     console.log("timePoint=" + finalPoint.timePoint);
+     console.log("deviceSerial=" + finalPoint.deviceSerial);
+     console.log("beaconCanculatedPosition=[{\"x\"=" + finalPoint.beaconCalculatePosition[0].x + ",\"y=\"" + finalPoint.beaconCalculatePosition[0].y + "}]");
+     //todo write back info the redis and trigger postback event using websocket
+     //Save to the redis
+     //SensorDataCalculator.saveToRedis(finalPoint);
+     });
+     }
+     });*/
 };
 
-SensorDataCalculator.getKeyBeforeCalculate=function(deviceSerial){
+SensorDataCalculator.getKeyBeforeCalculate = function (deviceSerial) {
     var timespan = (new Date()).getTime();
-    var key=null;
-    if(deviceSerial)
-    {
+    var key = null;
+    if (deviceSerial) {
         key = deviceSerial + "_" + timespan;
     }
     return key;
 };
 
-SensorDataCalculator.getKeyAfterCalculate=function(deviceSerial){
+SensorDataCalculator.getKeyAfterCalculate = function (deviceSerial) {
     var timespan = (new Date()).getTime();
-    var key=null;
-    if(deviceSerial)
-    {
-        key = deviceSerial + "_" + timespan+ "_" + "Calculated";
+    var key = null;
+    if (deviceSerial) {
+        key = deviceSerial + "_" + timespan + "_" + "Calculated";
     }
     return key;
 };
+
+SensorDataCalculator.processSingleLineCalculate = function (sourceData) {
+    var finalResult = [];
+    var dataObj;
+    try {
+        dataObj = JSON.parse(sourceData);
+        if (!dataObj.deviceSerial) {
+            return;
+        }
+    }
+    catch (e) {
+        console.log(e);
+        return;
+    }
+}
+
+SensorDataCalculator.filterDataByAcc = function (sourceData,filterValue) {
+    var tpPkg = sourceData.monitorPackage;
+    if (tpPkg) {
+        tpPkg.each(function (pkg) {
+            var tpTimeStamp = pkg.checkPoint;
+            var beaconNodes = pkg.beaconPKG;
+            if (beaconNodes&&beaconNodes.length > 3) {
+                    beaconNodes.each(function(tpNode){
+                        if(parseInt(tpNode.acc)>parseInt(filterValue)){
+                            beaconNodes.remove(tpNode);
+                        }
+                    });
+                pkg.beaconPKG=beaconNodes;
+            }
+        });
+    }
+    sourceData.monitoring=tpPkg;
+    return sourceData;
+}
 
 module.exports = SensorDataCalculator;
 
