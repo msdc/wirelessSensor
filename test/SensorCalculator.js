791573a33ac9d2b6dd88f2f658c647e42af7ffd6
donegal <murdercdh@hotmail.com>
2014-08-19 20:16:43 +0800

    add sample and test files

---
+++ test/SensorCalculator.js
@@ -0,0 +1,10 @@
+var sensorCalculator=require(".././SensorCalculator.js");
+var fs=require("fs");
+
+fs.readFile('../sampleData/data3.txt',{encoding:"UTF8",flag:"r"}, function (err, data) {
+    if (err) throw err;
+    //console.log(data);
+    var tpObj=JSON.parse(data);
+    var result= sensorCalculator.filterDataByAcc(data,"20");
+    console.log(result);
+});
