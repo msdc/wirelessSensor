f0649426d960d31691aeaaa88a664a77c99824b1
liuwenxiang <liuwenxiang0220@163.com>
2014-08-21 18:54:03 +0800

    new submit to server

---
+++ website/js/page/merchans-info.js
@@ -0,0 +1,24 @@
+﻿App.CurrentMenu = 'li-place-business';
+
+/*******************
+***     View    ***
+*******************/
+
+
+/*******************
+***     Model    ***
+*******************/
+
+
+
+/*******************
+***    Router    ***
+*******************/
+App.Router.map(function () {
+    this.resource('container', { path: '/' });
+});
+
+/*******************
+***  Initialize  ***
+*******************/
+App.initialize();
---
+++ website/js/page/place-position .js
@@ -0,0 +1,24 @@
+﻿App.CurrentMenu = 'li-place-location';
+
+/*******************
+***     View    ***
+*******************/
+
+
+/*******************
+***     Model    ***
+*******************/
+
+
+
+/*******************
+***    Router    ***
+*******************/
+App.Router.map(function () {
+    this.resource('container', { path: '/' });
+});
+
+/*******************
+***  Initialize  ***
+*******************/
+App.initialize();
