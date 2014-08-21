8952b595a7120489d473e3dfeaf4dd0b21bbbfdf
ycdeng <xiaoquer521@163.com>
2014-08-21 09:52:12 +0800

    添加网格与坐标系转换及其它功能。增加获取网格等相关注释。。

--- public/configScale.js
+++ public/configScale.js
@@ -1,4 +1,4 @@
-window.configJson={ //配置文件
+window.configJson={ //配置文件..将要转为ajax 获取json
     canvas:{
         w:600||700,h:600||700,//单位为像素
         numX:parseInt(4)<5?5:(parseInt(4)>9?9:parseInt(4)),//坐标系X轴个数..坐标点数最少为5，最多为9。。其中包括了(0,0)点
@@ -12,7 +12,8 @@ window.configJson={ //配置文件
     PPI:96||96, //1英寸有96px
     inchesM:0.0254, //1英寸等于0.0254米..固定值不可更改
     zoomImg:1,   //默认为1。。即图片和画布一样大小
-    uuidArr:function(){
+    uuidArr:function(){//这里只是给出一个格式。。将来以后台给出为主，或直接告知后台需要什么的数据和格式
+                       //locationCanvas.js中使用for in获取。。若修改这个的格式，同步修改locationCanvas.js
         var pointSB=[],
             frontStr='E2C56DB5-DFFB-48D2-B060-D0F5A71096E0';
         pointSB[frontStr+'_0_1']={x:250.00,y:310.00};//单位为米
