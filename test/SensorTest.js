e101f5b0e1079613d69d4f0f910eb1b04af524aa
donegal <murdercdh@hotmail.com>
2014-08-25 10:00:21 +0800

    add sample data define for bk mgmt

--- test/SensorTest.js
+++ test/SensorTest.js
@@ -6,7 +6,7 @@ var fs=require("fs");
 //
 //findPath.findPath(tpGraph,{x:0,y:0},{x:3,y:4});
 
-fs.readFile('../sampleData/data3.txt',{encoding:"UTF8",flag:"r"}, function (err, data) {
+fs.readFile('../sampleData/data3.json',{encoding:"UTF8",flag:"r"}, function (err, data) {
     if (err) throw err;
     //console.log(data);
     var tpObj=JSON.parse(data);
