a9cd4642fba5071b7ee8633b3db4d8dc8327e93a
donegal <murdercdh@hotmail.com>
2014-08-27 14:19:07 +0800

    update the device related func

--- webAPI/maintainModule.js
+++ webAPI/maintainModule.js
@@ -3,13 +3,8 @@
  */
 var redis = require('redis');
 var RedisOperator = require('./redisOperator.js');
-var config=require("./../config.js");
-
-var client = redis.createClient(config.redisSettings.port, config.redisSettings.host);
-// Get connection errors for logging...
-client.on("error", function (reason) {
-    console.log("Connection Error:", reason);
-})
+var config = require("./../config.js");
+var easypost = require('easypost');
 
 var place = function () {
 }
@@ -52,13 +47,70 @@ var beaconDevice = function () {
 }
 
 beaconDevice.prototype.add = function (req, res) {
-    var redisOperator = new RedisOperator(client, req, res);
-    redisOperator.Add('device');
+    var moduleName = "device";
+    easypost.get(req, res, function (data) {
+        if (data) {
+            var dataObj = typeof (data) == "object" ? data : JSON.parse(data);
+            var dataStr = typeof (data) == "string" ? data : JSON.stringify(data);
+            if (dataObj.uuid && dataObj.major && dataObj.minor) {
+                var client = redis.createClient(config.redisSettings.port, config.redisSettings.host);
+                // Get connection errors for logging...
+                client.on("error", function (reason) {
+                    console.log("Connection Error:", reason);
+                });
+                var tpkey = moduleName + "_" + dataObj.uuid + "_" + dataObj.major + "_" + dataObj.minor;
+                client.set(tpkey, dataStr);
+                res.send({result: "success"});
+            }
+            else {
+                res.send(500, {error: "data format error"});
+            }
+            client.quit();
+        }
+        else {
+            res.send(500, {error: "data format error"});
+        }
+    });
 }
 
 beaconDevice.prototype.get = function (req, res) {
-    var redisOperator = new RedisOperator(client, req, res);
-    redisOperator.Get('device');
+    var moduleName = "device";
+    var uuid = req.query.uuid;
+    var major = req.query.major;
+    var minor = req.query.minor;
+    var client = redis.createClient(config.redisSettings.port, config.redisSettings.host);
+    // Get connection errors for logging...
+    client.on("error", function (reason) {
+        console.log("Connection Error:", reason);
+    });
+    var keyPart = moduleName + "_*";
+    if (uuid && major && minor) {
+        keyPart = moduleName + "_" + uuid + "_" + major + "_" + minor;
+        client.get(tpkey, function (err, data) {
+            res.send(data);
+            client.quit();
+        });
+    }
+    else {
+        var result = [];
+        client.keys(keyPart, function (err, list) {
+            if (!err & list && list.length > 0) {
+                list.forEach(function (key, pos) {
+                    client.get(key, function (err, item) {
+                        result.push(item);
+                        if (pos == (list.length - 1)) {
+                            res.send(result);
+                            client.quit();
+                        }
+                    });
+                });
+            }
+            else {
+                res.send({error: "There is no device", message: err});
+                client.quit();
+            }
+        });
+    }
 }
 
 beaconDevice.prototype.del = function (req, res) {
