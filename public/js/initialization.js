041b6d1d04f4861bbaf5d0f70e36205f0f17718f
wang <wangliu@isoftstone.com>
2014-09-03 11:51:27 +0800

    add pages.

---
+++ public/js/initialization.js
@@ -0,0 +1,16 @@
+/**
+ * Created by wang on 2014/9/3.
+ */
+
+$(function(){
+    var btn_auto_deploy=$('#auto_deploy');
+    var btn_data_init=$('#data_init');
+
+    btn_auto_deploy.click(function(){
+
+    });
+
+    btn_data_init.click(function(){
+
+    });
+});
